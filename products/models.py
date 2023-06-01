bc218c808328856ee8500688ed95e536f1e96c3a
Oleg Semenov <semoleg1986@gmail.com>
2023-06-01 11:18:59 +0300

    feat: add graphene

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.name
+    class Meta:
+         ordering = ('name',)
+         verbose_name = 'Товар'
+         verbose_name_plural = 'Товары'
