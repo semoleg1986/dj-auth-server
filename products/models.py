01b26032dee578aa46b0785e73be458ca2d1f5c0
Oleg Semenov <semoleg1986@gmail.com>
2023-06-03 20:03:24 +0300

    feat: add category

--- products/models.py
+++ products/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -7,9 +18,12 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
+
     class Meta:
-         ordering = ('name',)
-         verbose_name = 'Товар'
-         verbose_name_plural = 'Товары'
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
--- products/schema.py
+++ products/schema.py
@@ -1,27 +1,40 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Product
+from .models import Product, Category
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
 
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
+    def resolve_category_name(self, info):
+        return self.category.name
+
 
 class Query(graphene.ObjectType):
     products = graphene.List(ProductType)
+    categories = graphene.List(CategoryType)
 
     def resolve_products(self, info):
         return Product.objects.all()
+    def resolve_categories(self, info):
+        return Category.objects.all()
+
 class CreateProduct(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
         description = graphene.String(required=True)
         price = graphene.Decimal(required=True)
         quantity = graphene.Int(required=True)
+        category_id = graphene.ID(required=True)
 
     product = graphene.Field(ProductType)
 
-    def mutate(self, info, name, description, price, quantity):
-        product = Product(name=name, description=description, price=price, quantity=quantity)
+    def mutate(self, info, name, description, price, quantity, category_id):
+        category = Category.objects.get(pk=category_id)
+        product = Product(name=name, description=description, price=price, quantity=quantity, category=category)
         product.save()
         return CreateProduct(product=product)
 
@@ -60,9 +73,46 @@ class DeleteProduct(graphene.Mutation):
         product.delete()
         return DeleteProduct(success=True)
 
+class CreateCategory(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, name):
+        category = Category(name=name)
+        category.save()
+        return CreateCategory(category=category)
+class UpdateCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, id, name):
+        category = Category.objects.get(pk=id)
+        category.name = name
+        category.save()
+        return UpdateCategory(category=category)
+
+class DeleteCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, id):
+        category = Category.objects.get(pk=id)
+        category.delete()
+        return DeleteCategory(success=True)
+
 class Mutation(graphene.ObjectType):
     create_product = CreateProduct.Field()
     update_product = UpdateProduct.Field()
     delete_product = DeleteProduct.Field()
+    create_category = CreateCategory.Field()
+    update_category = UpdateCategory.Field()
+    delete_category = DeleteCategory.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
