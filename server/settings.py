5dbf909af0f0b525bb634dd633c463c6eded80d2
Oleg Semenov <semoleg1986@gmail.com>
2023-06-10 23:03:45 +0300

    feat: add Postgres

--- server/settings.py
+++ server/settings.py
@@ -93,14 +93,28 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'obmasadb',
+        'USER': 'obmasadb',
+        'PASSWORD': '0uX4Ovrj78fOi9OarTyCHtCAzbdGTtHB',
+        'HOST': 'mel.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
+import dj_database_url
 
+elephantsql_url = 'postgres://obmasadb:0uX4Ovrj78fOi9OarTyCHtCAzbdGTtHB@mel.db.elephantsql.com/obmasadb'
+config = dj_database_url.parse(elephantsql_url)
+DATABASES['default'].update(config)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
